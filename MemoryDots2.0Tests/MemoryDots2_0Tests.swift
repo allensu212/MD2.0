2ed8b5d0718896e45088d270b60039237077509e
Allen Su <allensu212@gmail.com>
2015-11-21 23:52:11 -0500

    Initial Commit

---
+++ MemoryDots2.0Tests/MemoryDots2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MemoryDots2_0Tests.swift
+//  MemoryDots2.0Tests
+//
+//  Created by Wei-Lun Su on 11/21/15.
+//  Copyright (c) 2015 Allen Su. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MemoryDots2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
